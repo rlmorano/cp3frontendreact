f57e85b7e4aeff020d45b80177728390eed301ed
rlmorano <ralphlevigilhangmorano@gmail.com>
2020-01-19 19:42:32 +0800

    footer revised

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -5,18 +5,16 @@ import { Row, Container } from "reactstrap";
 
 function Footer() {
   return (
-    <footer className="footer bg-dark">
+    <footer className="footer-custom">
       <Container>
         <Row>
-          <div className="credits mx-auto">
+          <div className="credits mx-auto footer-custom mt-3">
             <span className="copyright">
               © December 2019 -- Capstone 3 created with{" "}
               <i className="fa fa-heart heart" /> By: R.L.M @ Zuitt Coding Bootcamp</span>
+            <br />
+            <br />
           </div>
-          <nav className="footer-nav">
-            <ul>
-            </ul>
-          </nav>
         </Row>
       </Container>
     </footer>
